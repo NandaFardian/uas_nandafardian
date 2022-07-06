d66f1bbe8b8948ed9f595ba8f39dd43379ff137a
NandaFardian <108007012+NandaFardian@users.noreply.github.com>
2022-07-06 20:17:27 +0700

    menambahkan table merk dan alatberat

--- database/migrations/2022_07_06_130734_create_merks_table.php
+++ database/migrations/2022_07_06_130734_create_merks_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('merks', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('hak_akses',['admin','user']);
-            $table->rememberToken();
+            $table->string('kode')->unique();
+            $table->string('merk');
+            $table->string('kapasitas');
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('merks');
     }
 };
